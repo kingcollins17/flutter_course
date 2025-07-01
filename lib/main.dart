7a7cdf41cf068004bcd9ce0b1cbe9ef900fab385
nanceeyngo <nanceeyngo@gmail.com>
2025-07-01 18:28:01 +0100

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,21 @@
-// ignore_for_file: avoid_print
+import 'package:flutter/material.dart';
+import 'package:flutter_course/newFile.dart';
 
-import 'dart:async';
+void main() {
+  runApp(const MyApp());
+}
 
-import 'async_try_catch_example.dart';
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          useMaterial3: true,
+        ),
+        home: newFile()
+    );
+  }
+}
---
+++ lib/newFile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class newFile extends StatelessWidget {
+  const newFile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
