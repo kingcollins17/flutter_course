cb4410df946bec0aea3028c256a5a80417edf586
davidShonibare <shonibaredavida@yahoo.com>
2025-07-19 16:18:01 +0100

    implement unit tests

---
+++ test/unit_test.dart
@@ -0,0 +1,91 @@
+import 'dart:io';
+
+import 'package:flutter_course/controllers/game_controller.dart';
+import 'package:flutter_course/models/guess_model.dart';
+import 'package:flutter_course/models/word_model.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:test/test.dart';
+
+Future setup() async {
+  var path = Directory.current.path; // or a custom test path
+  Hive.init(path);
+  Hive.registerAdapter(GuessModelAdapter());
+  await Hive.openBox('guesses');
+}
+
+class TestGameController extends GameController {
+  @override
+  void gameSnackBar(
+    String titleString,
+    String messageString, {
+    bool? isCorrect = false,
+    Duration? duration = const Duration(seconds: 2),
+  }) {}
+  @override
+  List<WordModel> get remainingWords => [
+    WordModel(
+      word: "love",
+      wordhints: [
+        "It means affection",
+        "Can mean 'like deeply'",
+        "Another word for fondness",
+      ],
+    ),
+  ];
+}
+
+void main() async {
+  late TestGameController controller;
+
+  setUpAll(() async {
+    await setup();
+  });
+  setUp(() {
+    controller = TestGameController();
+    controller.loadGame();
+  });
+  test('Secret Word Selection test', () async {
+    controller.getNewWord();
+    expect(controller.secretWord, isNotNull);
+  });
+  group('Testing the Guess', () {
+    test('Test Guess is right', () async {
+      controller.assessGuess('love');
+      expect(controller.isCorrect!, equals(true));
+    });
+    test('Test Guess is wrong', () async {
+      controller.assessGuess('like');
+      expect(controller.isCorrect!, equals(false));
+    });
+    test('Guess is only 4-letters', () async {
+      controller.assessGuess('coosl');
+      expect(controller.isCorrect, isNull);
+    });
+    test('Guess is not less or more than 4 letters ', () async {
+      controller.assessGuess('cool');
+      expect(controller.isCorrect, isNotNull);
+    });
+    test('Guess is only letters ', () async {
+      controller.assessGuess('c(1l');
+      expect(controller.isCorrect, isNull);
+    });
+  });
+
+  group('Test Points Award', () {
+    test('10 points added for each correct guess.', () async {
+      final initialScore = controller.currentScore.value;
+      controller.assessGuess('love');
+      expect(controller.currentScore.value, equals(initialScore + 10));
+    });
+    test('10 points not added for each wrong guess.', () async {
+      final initialScore = controller.currentScore.value;
+      controller.assessGuess('dove');
+      expect(controller.currentScore.value, equals(initialScore));
+    });
+  });
+  test('Guess History test', () async {
+    final initialHistoryLength = controller.previousGuess.length;
+    controller.assessGuess('love');
+    expect(controller.previousGuess.length, equals(initialHistoryLength + 1));
+  });
+}
